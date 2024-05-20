6e03dbe9b57ca1b4d1c64e9cb949d37805d84375
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-05-20 15:14:55 +0200

    Created ui component - loader field.

---
+++ src/components/ui/loader/loader.component.js
@@ -0,0 +1,28 @@
+import { $BS } from '@/core/bsquery/bsquery.lib'
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
+
+import styles from './loader.module.scss'
+import template from './loader.template.html'
+
+export const LOADER_SELECTOR = '[data-component="loader"]'
+
+export class Loader extends ChildComponent {
+	constructor(width = 100, height = 100) {
+		super()
+
+		this.width = width
+		this.height = height
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		$BS(this.element)
+			.css('width', `${this.width}px`)
+			.css('height', `${this.height}px`)
+			.addClass('bounce')
+
+		return this.element
+	}
+}
