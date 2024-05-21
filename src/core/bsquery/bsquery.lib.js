bc32c0f2e13900ba137e2a27001c03af9bbbc89b
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-05-21 19:05:50 +0200

    Added card info component.

--- src/core/bsquery/bsquery.lib.js
+++ src/core/bsquery/bsquery.lib.js
@@ -38,6 +38,16 @@ class BSQuery {
 		}
 	}
 
+	/**
+	 * Find all elements that match the specified selector within the selected element.
+	 * @param {string} selector - A CSS selector string to search for within the selected element.
+	 * @returns {BSQuery[]} An array of new RQuery instances for the found elements.
+	 */
+	findAll(selector) {
+		const elements = this.element.querySelectorAll(selector)
+		return Array.from(elements).map(element => new BSQuery(element))
+	}
+
 	// INSERT
 
 	/**
