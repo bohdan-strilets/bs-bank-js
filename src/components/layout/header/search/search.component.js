d829869cadf11f3c2684266e8a40657d8431264f
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-05-21 18:43:41 +0200

    Added methods for work with search.

--- src/components/layout/header/search/search.component.js
+++ src/components/layout/header/search/search.component.js
@@ -2,18 +2,61 @@ import { $BS } from '@/core/bsquery/bsquery.lib'
 import ChildComponent from '@/core/component/child.component'
 import renderService from '@/core/services/render.service'
 
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+
+import { debounce } from '@/utils/debaunce.uti'
+
+import { UserService } from '@/api/user.service'
+
 import styles from './search.module.scss'
 import template from './search.template.html'
 
 export class Search extends ChildComponent {
+	constructor() {
+		super()
+		this.userService = new UserService()
+	}
+
+	#handleSearch = async e => {
+		const searchTerm = e.target.value
+		const searchResultElement = $BS(this.element).find('#search-results')
+
+		if (!searchTerm) {
+			searchResultElement.html('')
+			return
+		}
+
+		await this.userService.getAll(searchTerm, users => {
+			searchResultElement.html('')
+			users.forEach((user, index) => {
+				const userItem = new UserItem(user, true, () => {
+					searchResultElement.html('')
+				}).render()
+				$BS(userItem)
+					.addClass(styles.item)
+					.css('transition-delay', `${index * 0.1}s`)
+
+				searchResultElement.append(userItem)
+
+				setTimeout(() => {
+					$BS(userItem).addClass(styles.visible)
+				}, 50)
+			})
+		})
+	}
+
 	render() {
 		this.element = renderService.htmlToElement(template, [], styles)
+		const debounceHandleSearch = debounce(this.#handleSearch, 300)
 
-		$BS(this.element).find('input').input({
-			type: 'search',
-			name: 'search',
-			placeholder: 'Search contacts...'
-		})
+		$BS(this.element)
+			.find('input')
+			.input({
+				type: 'search',
+				name: 'search',
+				placeholder: 'Search contacts...'
+			})
+			.on('input', debounceHandleSearch)
 
 		return this.element
 	}
